dec7e988df8c72f60d42157c46c0bfb0ea748876
Eugen Stan <eugen.stan@netdava.com>
2021-06-30 00:43:53 +0300

    Added statusRepl service

--- src/main/java/com/netdava/ofbiz/clojure/ClojureREPLService.java
+++ src/main/java/com/netdava/ofbiz/clojure/ClojureREPLService.java
@@ -2,7 +2,12 @@ package com.netdava.ofbiz.clojure;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
+import clojure.lang.Keyword;
+import java.net.ServerSocket;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.service.DispatchContext;
@@ -15,17 +20,24 @@ public class ClojureREPLService {
   public static final String MODULE = ClojureREPLService.class.getName();
   public static final ConcurrentHashMap<String, Object> CONTEXT = new ConcurrentHashMap<>();
   public static String port = "7888";
-  private static boolean started = false;
-  private static Object server = null;
+  // nrepl server implements java.util.Map (clojure defrecord)
+  private static Optional<Map<Object, Object>> server = Optional.empty();
 
   public static boolean isStarted() {
-    return started;
+    return server.isPresent();
   }
 
+  public static Map<String, ? extends Object> statusRepl(DispatchContext ctx,
+      Map<String, Object> context) {
+    context.put("status", getServerStats());
+    return context;
+  }
+
+  @SuppressWarnings("unchecked")
   public synchronized static Map<String, ? extends Object> startRepl(DispatchContext ctx,
       Map<String, ? extends Object> context) {
 
-    if (started) {
+    if (isStarted()) {
       Debug.logWarning("nRepl is already running on port " + port, MODULE);
     } else {
       Debug.logInfo("Starting nRrepl on port " + port, MODULE);
@@ -34,11 +46,14 @@ public class ClojureREPLService {
       IFn start = Clojure.var("nrepl.server", "start-server");
 
       CONTEXT.put("ofbiz-dispatch-context", ctx);
-      server = start.invoke(Clojure.read(":bind"), Clojure.read("\"localhost\""), Clojure.read(":port"),
-          Clojure.read(port));
+      Map s = (Map) start
+          .invoke(Clojure.read(":bind"), Clojure.read("\"localhost\""), Clojure.read(":port"),
+              Clojure.read(port));
+      server = Optional.of(s);
       // https://nrepl.org/nrepl/0.8/building_servers.html#basics
-      Debug.logInfo("nREPL server started on port " + port + " on host 127.0.0.1 - nrepl://127.0.0.1:" + port, MODULE);
-      started = true;
+      Debug.logInfo(
+          "nREPL server started on port " + port + " on host 127.0.0.1 - nrepl://127.0.0.1:" + port,
+          MODULE);
     }
 
     return context;
@@ -46,18 +61,32 @@ public class ClojureREPLService {
 
   public synchronized static Map<String, ? extends Object> stopRepl(DispatchContext ctx,
       Map<String, ? extends Object> context) {
-
-    if (started) {
+    if (isStarted()) {
       Debug.logInfo("Stopping nRrepl on port " + port, MODULE);
-      assert server != null;
       IFn stop = Clojure.var("nrepl.server", "stop-server");
-      stop.invoke(server);
+      Object s = server.get();
+      stop.invoke(s);
+      server = Optional.empty();
       Debug.logInfo("Stoppedn Rrepl " + port, MODULE);
-      started = false;
     } else {
       Debug.logInfo("nRepl is not running", MODULE);
     }
     return context;
+  }
+
+  static Map<String, Object> getServerStats() {
+    if (!server.isPresent()) {
+      return Collections.emptyMap();
+    }
+    ServerSocket ss = (ServerSocket) server.get().get(Keyword.find("server-socket"));
+
+    Map<String, Object> stats = new HashMap<>();
+    stats.put("bound", ss.isBound());
+    stats.put("closed", ss.isClosed());
+    stats.put("address", ss.getInetAddress().toString());
+    stats.put("port", ss.getLocalPort());
+
+    return stats;
   }
 
 }
